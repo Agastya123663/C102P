b0df6c3361238b2e65e87e0f78e431f52df9c6d4
Agastya123663 <68387647+Agastya123663@users.noreply.github.com>
2021-08-01 16:21:40 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,26 @@
+Amazing automation ideas that will help the world
+Most of these ideas are mechanical and cannot be programmed by one person but still with hard work everything is possible
+Here are some mind blowing ideas with the correct reference 
+
+1)
+ Geysers are important for everyday life , but sometimes we cannot switch on the geyser because of being in a meeting or any other reason
+That is why we can create an app that will help us to switch on the geyser from our car
+If you need more challenge , we can even add a feature to control the temperature and the intensity
+
+2)
+ On a hot day , an AC is very important especially for people driving in cars
+ But most ACs start giving the cooling effect after 10-15 minutes and we cant wait that long
+Thus we can create an app where we can switch on the ac in the car before even entering the car
+This is going to be very useful for all of us
+    
+3)
+Coffee is important for all of us , but it is too hechtic to go and make cofffee
+By using our phone , we can make coffee in the coffee making machine by choosing the amount of milk and powder etc..
+Thus we can create an app for the same
+
+4)
+Al of us forget to switch off the light when leaving the room and this wastes a lot of electricity
+We can thus program a sensor to automatically dim the lights when the lights are on
+The sensor will do this by seeing if any light energy and heat  is being produced or not and accordingly switch it off 
+
+   
